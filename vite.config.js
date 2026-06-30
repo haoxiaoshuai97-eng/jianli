381fcce43f48949b6944d53c8b254c215db3977f
郝晓帅 <934453059@qq.com>
2026-06-30 16:25:02 +0800

    configure custom domain

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 const repoName = process.env.GITHUB_REPOSITORY?.split('/')[1]
-const base = process.env.GITHUB_PAGES === 'true' && repoName ? `/${repoName}/` : '/'
+const base = process.env.GITHUB_PAGES_CUSTOM_DOMAIN === 'true'
+  ? '/'
+  : process.env.GITHUB_PAGES === 'true' && repoName
+    ? `/${repoName}/`
+    : '/'
 
 // https://vite.dev/config/
 export default defineConfig({
